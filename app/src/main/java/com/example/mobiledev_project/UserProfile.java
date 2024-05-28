167a5ff498a6f63b845552d2937bf71ea6c3a1a6
ShunMeizz <sebialshanleyflores@gmail.com>
2024-05-29 07:54:59 +0800

    coffin, urns, sendflowers

--- app/src/main/java/com/example/mobiledev_project/UserProfile.java
+++ app/src/main/java/com/example/mobiledev_project/UserProfile.java
@@ -1,14 +1,27 @@
 package com.example.mobiledev_project;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class UserProfile extends AppCompatActivity {
+    ImageView backButtonUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
+        backButtonUser = findViewById(R.id.backButtonUser);
+
+        backButtonUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(UserProfile.this, HomePage.class);
+                startActivity(intent1);
+            }
+        });
     }
 }
