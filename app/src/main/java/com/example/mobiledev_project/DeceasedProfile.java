be08a22a74d4de7585786b9aacd248d9cdaab64a
Jake <clarin.jake26@gmail.com>
2024-05-30 01:22:13 +0800

    all nighters club

--- app/src/main/java/com/example/mobiledev_project/DeceasedProfile.java
+++ app/src/main/java/com/example/mobiledev_project/DeceasedProfile.java
@@ -1,6 +1,11 @@
 package com.example.mobiledev_project;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,11 +14,42 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class DeceasedProfile extends AppCompatActivity {
+    ImageView backButtonOrbitiary;
+
+    // for database changes
+    TextView txtDeadHeader, txtDisplayFullName, txtDeadDate, txtDeadAbout;
+
+    // button pages
+    Button btnSendflower;
+    Button btnSendLove;
+    Button btnVisitShop;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_deceased_profile);
+
+        btnSendflower = (Button) findViewById(R.id.btnSendflower);
+        btnSendflower.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DeceasedProfile.this, SendFlowers.class);
+                startActivity(intent);
+            }
+        });
+
+        btnSendLove = (Button) findViewById(R.id.btnSendLove);
+
+
+        btnVisitShop = (Button) findViewById(R.id.btnVisitShop);
+        btnVisitShop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DeceasedProfile.this, SendFlowers.class);
+                startActivity(intent);
+            }
+        });
     }
 }
